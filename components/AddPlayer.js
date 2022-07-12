456f085687ffdee05a1b50f6a241031f44d9ee52
olivia pasion <oap9820@gmail.com>
2022-07-12 15:09:10 -0700

    adjusted select to reflect supabase items

--- components/AddPlayer.js
+++ components/AddPlayer.js
@@ -1,5 +1,6 @@
 
 export default function createAddPlayer(form, { handleAddPlayer }) {
+    const select = form.querySelector('select');
 
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
@@ -9,7 +10,30 @@ export default function createAddPlayer(form, { handleAddPlayer }) {
             formData.get('name'),
             formData.get('teamId')
         );
+        form.reset();
+        form.blur();
     });
     
-    return () => {};
+    return ({ teams }) => {
+        select.innerHTML = '';
+
+        const option = document.createElement('option');
+        option.disabled = true;
+        option.selected = true;
+        option.value = '';
+        option.textContent = 'Assign Subdivision';
+        select.append(option);
+        
+
+        for (const team of teams) {
+            select.append(Option({ team }));
+        }
+    };
+}
+
+function Option({ team }) {
+    const op = document.createElement('option');
+    op.value = team.id;
+    op.textContent = team.name;
+    return op;
 }
